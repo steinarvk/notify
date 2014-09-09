668f0a2ebc1a22c377189044002435ef2cfa579b
Steinar V. Kaldager <steinarvkaldager@gmail.com>
2014-09-09 17:48:49 +0200

    dynamic import workaround must do global import

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 class Services (object):
     def __getitem__(self, name):
         import importlib
-        template = "notify_{}"
+        template = "notify.notify_{}"
         module_name = template.format(name)
         return importlib.import_module(module_name)
 
